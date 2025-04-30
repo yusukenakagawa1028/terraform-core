6cbe632fc1d60a6a719bd44d8c7c250b186d5205
yusukenakagawa <yusukenakagawa7023@gmail.com>
2025-05-01 05:56:42 +0900

    Update dockerfile and script.

--- docker-image-build/entrypoint.sh
+++ docker-image-build/entrypoint.sh
@@ -2,6 +2,17 @@
 
 set -euo pipefail
 
+# リポジトリのルートに移動してrunnerをセットアップ
+./config.sh --url https://github.com/${GIT_REPO} \
+            --token "$GH_RUNNER_TOKEN" \
+            --unattended \
+            --labels ecs-runner
+
+# Actions runner を起動
+./run.sh
+
+
+
 if [ -z "${GITHUB_PAT:-}" ]; then
   echo "ERROR: GITHUB_PAT is not set"
   exit 1
@@ -16,13 +27,13 @@ GIT_BRANCH="${GIT_BRANCH:-main}"
 
 # Clone
 echo "Cloning Terraform code from GitHub..."
-git clone -b "$GIT_BRANCH" "https://${GITHUB_PAT}@github.com/${GIT_REPO}.git"
+git clone "https://github.com/${GIT_REPO}.git"
 
 # Terraform 実行
 cd "/enviroment/$ENV_DIR"
 
 echo "Running terraform init..."
-terraform init -input=false
+terraform init
 
 echo "Running terraform apply..."
 terraform apply -auto-approve
