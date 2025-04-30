046701f2fe708806e8392ea1ce8bf0c9ff2e333b
yusukenakagawa <yusukenakagawa7023@gmail.com>
2025-04-30 15:09:47 +0900

    Test execution github runner

--- enviroment/dev/main.tf
+++ enviroment/dev/main.tf
@@ -7,3 +7,9 @@ module "runner-setup" {
     region                 = "ap-northeast-1"
     github_runner_image_url = module.ecr.github_runner_image_url
 }
+
+resource "aws_eip" "test_eip" {
+  tags = {
+    Name = "test-eip"
+  }
+}
