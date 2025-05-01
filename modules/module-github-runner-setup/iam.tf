f99a6e2cb91903033110980344fa531182a501f1
yusukenakagawa <yusukenakagawa7023@gmail.com>
2025-05-01 15:03:15 +0900

    Add IAM policy.

--- modules/module-github-runner-setup/iam.tf
+++ modules/module-github-runner-setup/iam.tf
@@ -52,7 +52,45 @@ resource "aws_iam_policy" "secrets_access" {
   })
 }
 
+resource "aws_iam_policy" "execution_terraform" {
+  name = "GitHubRunnerExuecutionTerraformPolicy"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:ListBucket",
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ],
+        Resource = [
+          "arn:aws:s3:::pvdev-terraform-backend-bucket",
+          "arn:aws:s3:::pvdev-terraform-backend-bucket/*"
+        ]
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "kms:Decrypt",
+          "kms:Encrypt",
+          "kms:GenerateDataKey",
+          "kms:DescribeKey"
+        ],
+        Resource = "arn:aws:kms:${var.region}:${data.aws_caller_identity.current.account_id}:key/5d0fdd53-9f6a-4161-bd4a-5eeca512041f"
+      }
+    ]
+  })
+}
+
 resource "aws_iam_role_policy_attachment" "runner_task_policy_attach" {
   role       = aws_iam_role.runner_task_role.name
   policy_arn = aws_iam_policy.secrets_access.arn
+}
+
+resource "aws_iam_role_policy_attachment" "execution_terraform" {
+  role       = aws_iam_role.runner_task_role.name
+  policy_arn = aws_iam_policy.execution_terraform.arn
 }
